22870014c46a4e0813b2835b1126606627b1be5d
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2019-12-16 21:34:07 +0800

    check all

--- testparser.py
+++ testparser.py
@@ -9,6 +9,12 @@ INPUT_DIR = 'open_tests'
 files = sorted(os.listdir(INPUT_DIR))
 for fname in files:
     cmds = sygusparser.parser.parse(open(os.path.join(INPUT_DIR, fname)).read())
+    print(fname)
+    if cmds.cmd_set_logic:
+        print(cmds.cmd_set_logic)
+    for cmd in cmds.cmd_list:
+        print(cmd)
+    print()
     solver = sygussolver.SygusSolver()
     solver.solve(cmds)
 
