caf8c4feda90871cc6d6d54e4cc29bdc8a94bc49
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2019-12-16 22:54:45 +0800

    add test time

--- testparser.py
+++ testparser.py
@@ -3,19 +3,19 @@ import sygusparser
 import sygussolver
 
 import os
+import time
 
 INPUT_DIR = 'open_tests'
 
 # files = sorted(os.listdir(INPUT_DIR))
 files = ['max2.sl', 'three.sl', 'tutorial.sl']
 for fname in files:
-    cmds = sygusparser.parser.parse(open(os.path.join(INPUT_DIR, fname)).read())
     print(fname)
-    # if cmds.cmd_set_logic:
-    #     print(cmds.cmd_set_logic)
-    # for cmd in cmds.cmd_list:
-    #     print(cmd)
-    # print()
+    time0 = time.time()
+    cmds = sygusparser.parser.parse(open(os.path.join(INPUT_DIR, fname)).read())
+    time1 = time.time()
     solver = sygussolver.SygusSolver()
     solver.solve(cmds)
+    time2 = time.time()
+    print(f'Total: {time2-time0:.04f}s. Parse: {time1-time0:.04f}s. Solve: {time2-time1:.04f}s.')
 
